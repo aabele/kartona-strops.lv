71959eeff065eaf9df7fa3a3d692c2a0dfd6f745
Aivis Abele <aabele@gmail.com>
2018-04-19 09:09:46 +0300

    Improved wording.

--- backend/website/context_processors.py
+++ backend/website/context_processors.py
@@ -3,6 +3,6 @@
 def site_txt(request):
 
     return {
-        'greeting_txt': 'Gofrkartona bišu stropi ērtai bišu un atdaleņu transportēšanai - stropa svars ir tikai 800 '
-                        'grami. \n Noderēs visiem, kas ķer spietus, pērk, pārdod, pārvieto bites.'
+        'greeting_txt': 'Gofrkartona nukleusi ērtai bišu un atdaleņu transportēšanai - stropa svars ir tikai 800 '
+                        'grami. \n Noderēs visiem, kas ķer spietus, audzē, pērk, pārdod, pārvieto bites.'
     }
